ccc91c9b95f5489b5b7d51ab6be98347586ed38f
Jose Soria <josesoriap21@gmail.com>
2022-07-21 23:20:34 -0300

    1.2

--- src/pages/FoodsDetails.jsx
+++ src/pages/FoodsDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import products from "../assets/fake-data/products";
 import { useParams } from "react-router-dom";
@@ -6,64 +6,163 @@ import Helmet from "../components/Helmet/Helmet";
 import Commonsection from "../components/UI/common-section/CommonSection";
 import { Container, Row, Col } from "reactstrap";
 
-import productImg from "../assets/images/product_01.1.jpg";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../store/shopping-cart/cartSlice";
+
 import "../styles/product-details.scss";
 
+import ProductCard from "../components/UI/Product-card/ProductCard";
+
 const FoodsDetails = () => {
+  const [tab, setTab] = useState("desc");
+  const { id } = useParams();
+  const dispatch = useDispatch() 
+
+  const product = products.find((product) => product.id === id);
+  const [previewImg, setPreviewImg] = useState(product.image01);
+  const { title, price, category, desc, image01 } = product;
+
+  const relatedProduct = products.filter((item) => category === item.category);
+
+  const addItem = ()=>{
+    dispatch(cartActions.addItem({
+      id,
+      title,
+      price,
+      image01,
+
+    }))
+  }
+
+  useEffect(()=>{
+
+    setPreviewImg(product.image01)
+  },[product])
+
+  useEffect(()=>{
+    window.scrollTo(0,0)
+  },[product])
+
   return (
     <Helmet title="Product-details">
-      <Commonsection title="product 01" />
+      <Commonsection title={title} />
       <section>
         <Container>
           <Row>
             <Col lg="2" md="2">
               <div className="product__images">
-                <div className="img__item">
-                  <img src={productImg} alt="" className="w-50" />
+                <div
+                  className="img__item mb-3"
+                  onClick={() => setPreviewImg(product.image01)}
+                >
+                  <img src={product.image01} alt="" className="w-50" />
                 </div>
-                <div className="img__item">
-                  <img src={productImg} alt="" className="w-50" />
+                <div
+                  className="img__item mb-3"
+                  onClick={() => setPreviewImg(product.image02)}
+                >
+                  <img src={product.image02} alt="" className="w-50" />
                 </div>
-                <div className="img__item">
-                  <img src={productImg} alt="" className="w-50" />
+                <div
+                  className="img__item mb-3"
+                  onClick={() => setPreviewImg(product.image03)}
+                >
+                  <img src={product.image03} alt="" className="w-50" />
                 </div>
               </div>
             </Col>
 
             <Col lg="4" md="4">
               <div className="product__main-img">
-                <img src={productImg} alt="" className="w-100" />
+                <img src={previewImg} alt="" className="w-100" />
               </div>
             </Col>
 
             <Col lg="6" md="6">
               <div className="single__product-content">
-                <h2 className="product__title mb-3">Pizza with mushroom</h2>
+                <h2 className="product__title mb-3">{title}</h2>
                 <p className="product__price">
-                  Price: <span>$34</span>
+                  Price: <span>${price}</span>
                 </p>
                 <p className="category mb-5">
-                  Category: <span>Burger</span>
+                  Category: <span>{category}</span>
                 </p>
-                <button className="addTOCart__btn">Add to Cart</button>
+                <button onClick={addItem} className="addTOCart__btn" >Comprar</button>
               </div>
             </Col>
 
             <Col lg="12">
-              <div className="tabs d-flex align-items-center gap-3 py-2">
-                <h6>Descripción</h6>
-                <h6>Review</h6>
+              <div className="tabs d-flex align-items-center gap-5 py-3">
+                <h6
+                  className={`${tab === `desc` ? `tab__active` : ``} `}
+                  onClick={() => setTab("desc")}
+                >
+                  Descripción
+                </h6>
+                <h6
+                  className={`${tab === `rev` ? `tab__active` : ``} `}
+                  onClick={() => setTab("rev")}
+                >
+                  Review
+                </h6>
               </div>
 
-              <div className="tab__content">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed
-                  ea et accusamus ullam eius, eos est optio nam dicta quasi hic
-                  animi, minus ex quo earum quaerat quos! Iure veritatis saepe
-                  maxime. Iste quas suscipit sunt? Minus rerum a molestias.
-                </p>
-              </div>
+              {tab === "desc" ? (
+                <div className="tab__content">
+                  <p>{desc}</p>
+                </div>
+              ) : (
+                <div className="tab__form mb-3 ">
+                  <div className="review pt-5">
+                    <p className="user__name mb-0">Jhon Doe</p>
+                    <p className="user__email">jhon1@gmail.com</p>
+                    <p className="feedback__text">Great Product</p>
+                  </div>
+
+                  <div className="review">
+                    <p className="user__name mb-0">Jhon Doe</p>
+                    <p className="user__email">jhon1@gmail.com</p>
+                    <p className="feedback__text">Great Product</p>
+                  </div>
+
+                  <div className="review">
+                    <p className="user__name mb-0">Jhon Doe</p>
+                    <p className="user__email">jhon1@gmail.com</p>
+                    <p className="feedback__text">Great Product</p>
+                  </div>
+
+                  <form className="form">
+                    <div className="form__group">
+                      <input type="text" placeholder="Ingresa su numero" />
+                    </div>
+                    <div className="form__group">
+                      <input type="text" placeholder="Ingresa su numero" />
+                    </div>
+                    <div className="form__group">
+                      <textarea
+                        rows={5}
+                        type="text"
+                        placeholder="Ingresa su numero"
+                      />
+                    </div>
+
+                    <button type="submit" className="addTOCart__btn">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              )}
             </Col>
+
+            <Col lg="12" className="mb-5 mt-4">
+              <h2 className="related__Product-title">También podría interesarte...</h2>
+            </Col>
+
+            {relatedProduct.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" className="mb-4" key={item.id}>
+                <ProductCard item={item} />
+              </Col>
+            ))}
           </Row>
         </Container>
       </section>
